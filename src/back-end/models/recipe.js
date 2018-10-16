958135cc530dc16207d68f415c1a3438cf1e55b8
JoeNg93 <ntuandung93@gmail.com>
2018-10-16 10:11:30 +0300

    Add Recipe db schema

---
+++ src/back-end/models/recipe.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const { pick } = require('lodash');
+
+const RecipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  image: {
+    _id: String,
+    versionId: String,
+    imageType: String,
+  },
+  ingredients: [
+    {
+      _id: String,
+      quantity: String,
+      name: String,
+    },
+  ],
+  cookingTime: Number,
+  numOfMeals: Number,
+  instructions: [String],
+  numOfLikes: {
+    type: Number,
+    default: 0,
+  },
+  numOfDislikes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Recipe = mongoose.model('Recipe', RecipeSchema);
+
+RecipeSchema.methods.serialize = function serialize() {
+  return pick(this, [
+    'name',
+    'description',
+    'image',
+    'ingredients',
+    'cookingTime',
+    'numOfMeals',
+    'instructions',
+    'numOfLikes',
+    'numOfDislikes',
+  ]);
+};
+
+module.exports = Recipe;
