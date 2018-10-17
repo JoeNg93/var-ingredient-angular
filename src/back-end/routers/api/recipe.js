4a3d4a5520f9064fb555eda656ad80c5d7317ff8
JoeNg93 <ntuandung93@gmail.com>
2018-10-18 01:41:19 +0300

    Add API to like/dislike a recipe

--- src/back-end/routers/api/recipe.js
+++ src/back-end/routers/api/recipe.js
@@ -42,4 +42,46 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.patch('/:id/likes', async (req, res) => {
+  const { id } = req.params;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).json({ error: 'id is invalid.' });
+  }
+
+  try {
+    const recipe = await Recipe.findById(id);
+    if (!recipe) {
+      return res.status(404).send();
+    }
+    recipe.numOfLikes += 1;
+    recipe.save();
+    return res.status(204).send();
+  } catch (err) {
+    logger.log('error', `Error in /api/recipes: ${err.message}`);
+    return res.status(500).send();
+  }
+});
+
+router.patch('/:id/dislikes', async (req, res) => {
+  const { id } = req.params;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).json({ error: 'id is invalid.' });
+  }
+
+  try {
+    const recipe = await Recipe.findById(id);
+    if (!recipe) {
+      return res.status(404).send();
+    }
+    recipe.numOfDislikes += 1;
+    recipe.save();
+    return res.status(204).send();
+  } catch (err) {
+    logger.log('error', `Error in /api/recipes: ${err.message}`);
+    return res.status(500).send();
+  }
+});
+
 module.exports = router;
